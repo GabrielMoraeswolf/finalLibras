afdbe7805ac703773dd2958b82f35798ab7934b0
GabrielMoraeswolf <58598993+GabrielMoraeswolf@users.noreply.github.com>
2023-08-13 23:31:39 -0300

    git add

--- src/SquareGridData.js
+++ src/SquareGridData.js
@@ -1,8 +1,8 @@
 const dataArray = [
     { id: '1', value: 'Rocha', imageLink: "Rocha.gif"  },
-    { id: '2', value: '.', imageLink: 'imagem2.gif'  },
+    { id: '2', value: 'Geologia', imageLink: 'Geologia.gif'  },
     { id: '3', value: 'Fóssil', imageLink: 'fossil.gif'  },
-    { id: '4', value: '.', imageLink: 'imagem4.gif'  },
+    { id: '4', value: 'Vulcanismo', imageLink: 'Vulcanismo.gif'  },
     { id: '5', value: 'Rocha Ígnea', imageLink: 'Rochaignea.gif'  },
     { id: '6', value: 'Erosão', imageLink: 'Erosao.gif'  },
     { id: '7', value: 'Aquífero', imageLink: 'aquifero.gif' },
@@ -10,11 +10,11 @@ const dataArray = [
     { id: '9', value: 'Rocha Sedimentar', imageLink: 'RochaSedimentar.gif'  },
     { id: '10', value: 'Terremoto', imageLink: 'Terremoto.gif' },
     { id: '11', value: 'Rocha Metamórfica', imageLink: 'RochaMetamorfica.gif' },
-    { id: '12', value: '.', imageLink: 'imagem12.gif' },
-    { id: '13', value: '.', imageLink: 'imagem13.gif' },
-    { id: '14', value: '.', imageLink: 'imagem14.gif' },
-    { id: '15', value: '.', imageLink: 'imagem15.gif' },
-    { id: '16', value: '.', imageLink: 'imagem16.gif' },
+    { id: '12', value: 'Placa Tectônica', imageLink: 'PlacaTectonica.gif' },
+    { id: '13', value: 'Mapa', imageLink: 'Mapa.gif' },
+    { id: '14', value: 'Camadas do Planeta', imageLink: 'Camadas.gif' },
+    { id: '15', value: 'Núcleo Terrestre', imageLink: 'NucleoTerrestre.gif' },
+    { id: '16', value: 'Ciência da Computação', imageLink: 'CienciaComputacao.gif' },
     
   ];
 
